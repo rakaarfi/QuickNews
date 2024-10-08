ccfc1e41fad09fef8039488f76ab1621b7230f39
rakaarfi <rakaarfi@gmail.com>
2024-10-08 10:22:04 +0700

    Changing function and add concurrent

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import requests
-from prettyprinter import pprint as pp
+import concurrent.futures
 
-from requesting import get_requests
-from parsing import get_link, get_info_all_links
-from exporting import create_dict
+# (Nama folder) + (.) + (nama file) import 
+from NewsAggregator.requesting import get_requests
+from NewsAggregator.parsing import get_link, get_info_all_links
+from NewsAggregator.exporting import create_dict
 
-def main(url):
-  # Create a session for making HTTP requests.
-  session = requests.Session()
-
+def scraping_per_page(url, session):
   # Sends requests with a session.
   soup, status = get_requests(url, session)
 
@@ -16,13 +14,41 @@ def main(url):
   link = get_link(soup)
 
   # Extract detailed information and its index from all link.
-  all_info, index = get_info_all_links(link)
+  # all_info, index = get_info_all_links(link)
 
   # Store all info to a dictionary.
-  result = create_dict(resource=url, count=index+1, data=all_info)
+  # result = create_dict(resource=url, count=index+1, data=link)
 
-  return result
+  return link
+
+def scraping_multi_page(base_url):
+  results = []
+  #base_url = 'https://news.detik.com/indeks/'
+
+  session = requests.Session()
+
+  for i in range(1):
+    url = f"{base_url+str(i)}"
+    print(url)
+
+    # Store all info to a dictionary.
+    result = scraping_per_page(url, session=session)
+
+    # Add the result in the empty list
+    results.extend(result)
+
+  # Extract detailed information and its index from all link.
+  all_info, index = get_info_all_links(results)
+
+  with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    executor.map(get_info_all_links, results)
+
+  # Store all info to a dictionary.
+  data = create_dict(resource=base_url, count=index+1, data=all_info)
+
+  return data
 
 if __name__ == '__main__':
   url = 'https://news.detik.com/indeks'
-  result = main(url)
+  result = scraping_multi_page(url)
+  print(result)
